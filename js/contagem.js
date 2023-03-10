20679c49cc6070852177f02229e48648fd814fad
hsandinha <hebertsandinha@gmail.com>
2023-03-10 16:34:48 -0300

    ..

--- js/contagem.js
+++ js/contagem.js
@@ -1,8 +1,13 @@
-// Define a data final da contagem regressiva
-var countDownDate = new Date("Mar 10, 2023 17:00:00").getTime();
+function contagem(){
+// Obtém o elemento de entrada e o elemento de saída
+var countdownInput = document.getElementById("dtcontagem");
+var countdownOutput = document.getElementById("countdown");
 
 // Atualiza a contagem regressiva a cada segundo
 var x = setInterval(function() {
+
+  // Obtém a data final da contagem regressiva do elemento de entrada
+  var countDownDate = new Date(countdownInput.value).getTime();
 
   // Obtém a data e hora atual
   var now = new Date().getTime();
@@ -17,11 +22,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Exibe a contagem regressiva na página HTML
-  document.getElementById("countdown").innerHTML = days + "Dias - " + hours + "h " + minutes + "m "  + seconds + "s" ;
+  countdownOutput.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // Quando a contagem regressiva termina, exibe uma mensagem
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "A contagem regressiva terminou!";
+    countdownOutput.innerHTML = "A contagem regressiva terminou!";
   }
 }, 1000);
+
+}
