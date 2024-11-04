9179474476e0cb5b0952ad34d704ae06a24065f1
Hebert Sandinha <hebertsandinha@gmail.com>
2024-11-04 09:33:20 -0300

    Update contagem.js

--- js/contagem.js
+++ js/contagem.js
@@ -22,7 +22,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Exibe a contagem regressiva na página HTML
-  countdownOutput.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  countdownOutput.innerHTML = dias + "d " + horas + "h " + minutos + "m " + segundos + "s ";
 
   // Quando a contagem regressiva termina, exibe uma mensagem
   if (distance < 0) {
